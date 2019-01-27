10075f5841c035d766479d4754f53a70756ad9ea
soramugi <soramugi.chika@gmail.com>
2019-01-27 10:46:32 +0900

    s3アップに対応

--- vue.config.js
+++ vue.config.js
@@ -9,18 +9,10 @@ module.exports = {
         win: {
           icon: 'build/icon.png',
         },
-        // publish: {
-        //   provider: 's3',
-        //   bucket: 'example-vue-electron',
-        //   region: 'ap-northeast-1',
-        //   acl: 'public-read',
-        //   "x-amz-acl": "public-read",
-        // },
         publish: {
-          provider: 'generic',
-          url: 'http://soramugi.xsrv.jp/electron/',
-          publishAutoUpdate: true,
-          channel: 'latest'
+          provider: 's3',
+          bucket: 'example-vue-electron',
+          region: 'ap-northeast-1',
         },
       }
     }
